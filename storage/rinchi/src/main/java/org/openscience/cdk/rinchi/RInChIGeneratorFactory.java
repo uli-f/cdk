9b3e0e3ce9ec019bba4ea34be51514544740da9d
ntk73 <nick@uni-plovdiv.net>
2022-07-28 16:56:12 +0300

    Implemented basic functions in RInChIGeneratorFactory

--- storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIGeneratorFactory.java
+++ storage/rinchi/src/main/java/org/openscience/cdk/rinchi/RInChIGeneratorFactory.java
@@ -18,32 +18,79 @@
  */
 package org.openscience.cdk.rinchi;
 
+import java.util.List;
+
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IReaction;
+
+import io.github.dan2097.jnarinchi.RinchiFlag;
+import io.github.dan2097.jnarinchi.RinchiOptions;
 
 public class RInChIGeneratorFactory {
-	
+
 	private static RInChIGeneratorFactory INSTANCE;
-	
+
 	private RInChIGeneratorFactory() throws CDKException {
-    }
-	
-	
+	}
+
+
 	/**
-     * Gives the one <code>RInChIGeneratorFactory</code> instance,
-     * if needed also creates it.
-     *
-     * @return the one <code>RInChIGeneratorFactory</code> instance
-     * @throws CDKException if unable to load native code when attempting
-     *                      to create the factory
-     */
-    public static RInChIGeneratorFactory getInstance() throws CDKException {
-        synchronized (RInChIGeneratorFactory.class) {
-            if (INSTANCE == null) {
-                INSTANCE = new RInChIGeneratorFactory();
-            }
-            return INSTANCE;
-        }
-    }
-	
-	
+	 * Gives the one <code>RInChIGeneratorFactory</code> instance,
+	 * if needed also creates it.
+	 *
+	 * @return the one <code>RInChIGeneratorFactory</code> instance
+	 * @throws CDKException if unable to load native code when attempting
+	 *                      to create the factory
+	 */
+	public static RInChIGeneratorFactory getInstance() throws CDKException {
+		synchronized (RInChIGeneratorFactory.class) {
+			if (INSTANCE == null) {
+				INSTANCE = new RInChIGeneratorFactory();
+			}
+			return INSTANCE;
+		}
+	}
+
+	/**
+	 * Gets a Standard RInChI generator for a {@link IReaction}. 
+	 *
+	 * @param container Reaction to generate RInChI for.
+	 * @return the RInChI generator object
+	 * @throws CDKException if the generator cannot be instantiated
+	 */
+	public RInChIGenerator getRInChIGenerator(IReaction reaction) throws CDKException {
+		return (new RInChIGenerator(reaction));
+	}
+
+	/**
+	 * Gets a RInChI generator for a {@link IReaction} providing flags to customise the generation. If you
+	 * need to provide a timeout the method that accepts an {@link io.github.dan2097.jnainchi.InchiOptions}
+	 * should be used.
+	 * 
+	 * @param reaction Reaction to generate RInChI for.
+	 * @param flags the option flags
+	 * @return the RInChI generator object
+	 * @throws CDKException if the generator cannot be instantiated
+	 */
+	public RInChIGenerator getRInChIGenerator(IReaction reaction, RinchiFlag...flags) throws CDKException {
+		if (flags == null) throw new IllegalArgumentException("Null flags");
+		RinchiOptions options = new RinchiOptions.RinchiOptionsBuilder()
+				.withFlag(flags)
+				.build();
+		return getRInChIGenerator(reaction, options);
+	}
+
+	/**
+	 * Get a RInChI generator providing flags to customise the generation.
+	 * @param reaction Reaction to generate RInChI for
+	 * @param options the rinchi option flags
+	 * @return the RInChI generator
+	 * @throws CDKException something went wrong
+	 */
+	public RInChIGenerator getRInChIGenerator(IReaction reaction, RinchiOptions options) throws CDKException {
+		if (options == null) throw new IllegalArgumentException("Null flags");
+		return (new RInChIGenerator(reaction, options));
+	}
+
+
 }
