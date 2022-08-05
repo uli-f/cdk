4645ea778b756f85d33006d6aa6164bbfc114e50
ntk73 <nick@uni-plovdiv.net>
2022-08-05 13:45:45 +0300

    Implemented double rinchi conversion test helper functions. Added example test case

--- storage/rinchi/src/test/java/org/openscience/cdk/rinchi/RInChIToReactionTest.java
+++ storage/rinchi/src/test/java/org/openscience/cdk/rinchi/RInChIToReactionTest.java
@@ -19,12 +19,66 @@
 
 package org.openscience.cdk.rinchi;
 
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openscience.cdk.interfaces.IReaction;
 import org.openscience.cdk.test.CDKTestCase;
 import org.openscience.cdk.tools.ILoggingTool;
 import org.openscience.cdk.tools.LoggingToolFactory;
 
+import io.github.dan2097.jnarinchi.RinchiOptions;
+import io.github.dan2097.jnarinchi.RinchiStatus;
+
+
 public class RInChIToReactionTest extends CDKTestCase {
 	
 	private static final ILoggingTool logger = LoggingToolFactory.createLoggingTool(RInChIToReactionTest.class);
+	
+	public void doubleConversionTest(String rinchi, String auxInfo, boolean useCDK_MDL_IO, boolean useCDK_MDL_IO2) throws Exception {
+		//RInChI --> Reaction
+		RInChIToReaction r2r = RInChIGeneratorFactory.getInstance().getRInChIToReaction(rinchi, auxInfo, useCDK_MDL_IO);
+		Assert.assertEquals("RInChI status:",RinchiStatus.SUCCESS, r2r.getStatus());
+		IReaction reaction = r2r.getReaction();
+		Assert.assertNotNull(reaction);
+		//Reaction --> RInChI
+		RInChIGenerator gen = RInChIGeneratorFactory.getInstance().
+			 	getRInChIGenerator(reaction, RinchiOptions.DEFAULT_OPTIONS, useCDK_MDL_IO2);
+		Assert.assertNotNull(gen);
+		Assert.assertEquals("RInChI status:",RinchiStatus.SUCCESS, gen.getRInChIStatus());
+		Assert.assertEquals("RinChI:", rinchi, gen.getRInChI());
+		if (!auxInfo.isEmpty()) 
+			Assert.assertEquals("RAuxInfo:", auxInfo, gen.getAuxInfo());
+	}
+	
+	public void doubleConversionTestForExampleFile(String rinchiFile, boolean useCDK_MDL_IO, boolean useCDK_MDL_IO2) throws Exception {
+		//RInChI --> Reaction
+		Map<String, String> rfi = RInChIGeneratorTest.readRinchiFullInfoFromResourceFile(rinchiFile);
+		String rinchi = rfi.get("RInChI");
+		String auxInfo = rfi.get("RAuxInfo");
+		RInChIToReaction r2r = RInChIGeneratorFactory.getInstance().getRInChIToReaction(rinchi, auxInfo, useCDK_MDL_IO);
+		Assert.assertEquals("RInChI status:",RinchiStatus.SUCCESS, r2r.getStatus());
+		IReaction reaction = r2r.getReaction();
+		Assert.assertNotNull(reaction);
+		//Reaction --> RInChI 
+		RInChIGenerator gen = RInChIGeneratorFactory.getInstance().
+			 	getRInChIGenerator(reaction, RinchiOptions.DEFAULT_OPTIONS, useCDK_MDL_IO2);
+		Assert.assertNotNull(gen);
+		Assert.assertEquals("RInChI status:",RinchiStatus.SUCCESS, gen.getRInChIStatus());
+		Assert.assertEquals("RinChI:", rinchi, gen.getRInChI());
+		Assert.assertEquals("RAuxInfo:", auxInfo, gen.getAuxInfo());
+		Assert.assertEquals("Long-RInChIKey:", rfi.get("Long-RInChIKey"), gen.getLongRInChIKey());
+		Assert.assertEquals("Short-RInChIKey:", rfi.get("Short-RInChIKey"), gen.getShortRInChIKey());
+		Assert.assertEquals("Web-RInChIKey:", rfi.get("Web-RInChIKey"), gen.getWebRInChIKey());
+	}
+	
+	@Test
+	public void testExample_Tautomerization_01() throws Exception {
+		doubleConversionTestForExampleFile("examples/Tautomerization_01.txt", false, false);
+		doubleConversionTestForExampleFile("examples/Tautomerization_01.txt", true, true);
+		//doubleConversionTestForExampleFile("examples/Tautomerization_01.txt", false, true);
+		doubleConversionTestForExampleFile("examples/Tautomerization_01.txt", true, false);
+	}
 	
 }
