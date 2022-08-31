b1dccf8657d6427ab23fd5089f69a1a3930260ed
ntk73 <nick@uni-plovdiv.net>
2022-08-31 15:03:18 +0300

    Implemented function cdkStereoElementToInchiStereo()

--- storage/rinchi/src/test/java/org/openscience/cdk/rinchi/RInChIToReactionTest.java
+++ storage/rinchi/src/test/java/org/openscience/cdk/rinchi/RInChIToReactionTest.java
@@ -114,9 +114,9 @@ public class RInChIToReactionTest extends CDKTestCase {
 	@Test
 	public void testExample_Inverted_stereochemistry() throws Exception {		
 		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", true, true);
-		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", false, false, false);
+		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", false, false, false); //RAuxInfo is not compared
 		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", false, true, false); //RAuxInfo is not compared
-		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", true, false, false); //RAuxInfo is not compared
+		doubleConversionTestForExampleFile("examples/Inverted_stereochemistry.txt", true, false); 
 	}
 	
 	@Test
